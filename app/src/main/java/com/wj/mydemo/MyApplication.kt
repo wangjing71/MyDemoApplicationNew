99c58f1c44712fd0cea1f0912a17b55e8e8f1801
WangJing <397383523@qq.com>
2019-12-18 11:02:13 +0800

    kotlin master

---
+++ app/src/main/java/com/wj/mydemo/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.wj.mydemo
+
+import android.app.Application
+
+/**
+ * author wangjing
+ * Date 2018/11/29
+ * Description
+ */
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
