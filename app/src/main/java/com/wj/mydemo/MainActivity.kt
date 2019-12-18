6dbcc2809c14e9fb62ceec912a2e339f32042f9b
WangJing <397383523@qq.com>
2019-12-18 11:15:36 +0800

    kotlin master

--- app/src/main/java/com/wj/mydemo/MainActivity.kt
+++ app/src/main/java/com/wj/mydemo/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.wj.mydemo
 
+import android.widget.EditText
+import android.R.id
+import butterknife.BindView
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.Button
+
+
 class MainActivity : BaseActivity() {
+    @BindView(R.id.button)
+    var button: Button? = null
+
     override fun setLayoutId(): Int {
         return R.layout.activity_main
     }
